17406c94115cc587ec378ec2722f9d34a44ca93a
Bilal Kuzey <bk1405bgl@gmail.com>
2023-03-19 17:11:35 +0100

    outsourced repeating codes in components, like the tags, or company entries (cards)

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,20 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
+
+@section('content')
+@include('partials._spot')
+@include('partials._search')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+@unless(count($listings) == 0)
+
 @foreach($listings as $listing)
-<h3>{{$listing['title']}}</h3>
-<p>{{$listing['description']}}</p>
+    <x-listing-card :listing="$listing" />
 @endforeach
+
+@else
+<p>
+    No listings found
+</p>
+@endunless
+    </div>
+@endsection
